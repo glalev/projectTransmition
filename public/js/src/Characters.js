b856f3d011edf81236901bdf081e80a59f6415ca
paperjack93 <s.sottocorna@gmail.com>
2018-01-28 03:47:03 +0200

    Characters polish

--- public/js/src/Characters.js
+++ public/js/src/Characters.js
@@ -20,6 +20,11 @@ class Characters extends PIXI.Container {
       this.characters[2],
       this.characters[3]
     );
+
+    this.characters.forEach((char, index)=>{
+      this.playIdle(index);
+    })
+
   }
 
   addCharacter(x,y, flip){
@@ -34,7 +39,7 @@ class Characters extends PIXI.Container {
       character.tiredSprite.visible = false;
 
       character.addChild(character.idleSprite, character.actionSprite0, character.actionSprite1, character.tiredSprite);
-
+      character.isFlipped = flip;
       character.lastDir = false;
       character.x = x;
       character.y = y;
@@ -46,11 +51,22 @@ class Characters extends PIXI.Container {
   }
 
   playIdle(index){
-    TweenMax.fromTo(this.characters[index].transform.scale, 0.6, {x: 0.67}, {x:0.63, repeat: -1, yoyo: true});
+    let character = this.characters[index];
+
+    character.idleSprite.visible = true;
+    character.actionSprite0.visible = false;
+    character.actionSprite1.visible = false;
+    character.tiredSprite.visible = false;
+
+    if(character.tiredTween) character.tiredTween.kill();
+    if(character.idleTween) character.idleTween.kill();
+
+    character.idleTween = TweenMax.fromTo(character.scale, 0.6, {x: character.isFlipped ? -0.57 : 0.57}, {x:character.isFlipped ? -0.56 : 0.56, repeat: -1, yoyo: true});
   }
 
   playButton(index){
     let character = this.characters[index];
+
     character.idleSprite.visible = false;
     character.actionSprite0.visible = false;
     character.actionSprite1.visible = false;
@@ -60,18 +76,45 @@ class Characters extends PIXI.Container {
       character.actionSprite0.visible = true;
       TweenMax.fromTo(character.skew, 0.2, {x: -0}, {x:0, 
         onComplete: ()=>{
-        character.actionSprite0.visible = false;
-        character.idleSprite.visible = true;
+          character.actionSprite0.visible = false;
+          character.idleSprite.visible = true;
+          this.playIdle(index);
       }});
     } else {
       character.actionSprite1.visible = true;
       TweenMax.fromTo(character.skew, 0.2, {x: 0}, {x:0, 
         onComplete: ()=>{
-        character.actionSprite1.visible = false;
-        character.idleSprite.visible = true;
+          character.actionSprite1.visible = false;
+          character.idleSprite.visible = true;
+          this.playIdle(index);
       }});
     }
+
+    if(character.tiredTween) character.tiredTween.kill();
+    if(character.idleTween) character.idleTween.kill();
+
     character.lastDir = !character.lastDir;
   }
+
+  playTiredAll(){
+    this.characters.forEach((char, index)=>{
+      this.playTired(index);
+    })
+  }
+
+  playTired(index){
+    let character = this.characters[index];
+
+    character.idleSprite.visible = false;
+    character.actionSprite0.visible = false;
+    character.actionSprite1.visible = false;
+    character.tiredSprite.visible = true;
+    if(character.tiredTween) character.tiredTween.kill();
+    if(character.idleTween) character.idleTween.kill();
+
+    character.tiredTween = TweenMax.fromTo(character.scale, 0.5, {y:0.57}, {y:0.56, yoyo: true, repeat: -1});
+    character.tiredTween.timeScale(0.8 + (Math.random()*0.2));
+  }
+
 }
 module.exports = Characters;
